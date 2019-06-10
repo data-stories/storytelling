ec7bde7947a24d99643c974793ba4cf0214c3eaf
Tom Blount <t.blount@soton.ac.uk>
2019-06-10 11:55:18 +0100

    Method skeletons

--- javascript/data.js
+++ javascript/data.js
@@ -5,4 +5,21 @@ class Data {
     this.headers = [];
     this.headersOfInterest = headersOfInterest;
   }
+
+
+  detectTrends(){
+      throw new Error("Not Implemented")
+  }
+
+  detectSpikes(){
+      throw new Error("Not Implemented")
+  }
+
+  recommendCharts(){
+      throw new Error("Not Implemented")
+  }
+
+  recommendStructure(){
+      throw new Error("Not Implemented")
+  }
 }
