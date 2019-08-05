be690c98b6a75066455cc60d970877635dad3557
Tom Blount <t.blount@soton.ac.uk>
2019-08-05 11:19:52 +0100

    Added some recommender requirements

--- javascript/management/recommender.js
+++ javascript/management/recommender.js
@@ -1,3 +1,16 @@
 function getRecommendedCharts(){
+
+    //TODO: Recommend charts
+
+    //TODO: Recommend sensible charts
+
+    //TODO: Recommend a mixture of bar/line charts
+
+    //TODO: Weight recommended charts based on template
+
+    //TODO: Weight recommended charts based on previously selected charts
+
+    //TODO: Prevent recommender from recommending existing charts
+
     return ["Chart 1", "Chart 2", "Chart 3"]
 }
