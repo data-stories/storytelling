8dc6313aee1eaac77fabe469d26b1b0ff0f8c227
Yuchen Zhao <yuchen.bear@gmail.com>
2019-09-04 19:07:18 +0100

    demo data updated; a bug in chart recommendation fixed

--- javascript/management/recommender.js
+++ javascript/management/recommender.js
@@ -1,126 +1,130 @@
-function getRecommendedCharts(){
+function getRecommendedCharts() {
+  var charts = [];
 
-    var charts = [];
+  charts = charts.concat(chartsInterestDependency());
 
-    charts = charts.concat(chartsInterestDependency());
+  charts = charts.concat(chartsInterests());
 
-    charts = charts.concat(chartsInterests());
+  // Story.instance.metadata.dependencies.forEach(function(dependency){
 
-    // Story.instance.metadata.dependencies.forEach(function(dependency){
+  // });
 
-    // });
+  //TODO: Recommend charts
 
-    //TODO: Recommend charts
+  //TODO: Recommend sensible charts
 
-    //TODO: Recommend sensible charts
+  //TODO: Recommend a mixture of bar/line charts
 
-    //TODO: Recommend a mixture of bar/line charts
+  //TODO: Weight recommended charts based on template
 
-    //TODO: Weight recommended charts based on template
+  //TODO: Weight recommended charts based on previously selected charts
 
-    //TODO: Weight recommended charts based on previously selected charts
+  //TODO: Prevent recommender from recommending existing charts
 
-    //TODO: Prevent recommender from recommending existing charts
-
-    return charts;
+  return charts;
 }
 
 /**
  * Recommends an array of charts based on authors expressed interests and potential dependencies.
  * The type of each recommended chart depends on the type of x-axis.
  */
-function chartsInterestDependency(){
-    var interests = Story.instance.metadata.interests;
-    var dependencies = Story.instance.metadata.dependencies;
+function chartsInterestDependency() {
+  var interests = Story.instance.metadata.interests;
+  var dependencies = Story.instance.metadata.dependencies;
 
-    var charts = [];
-    for(var i=0; i< dependencies.length; i++){
-        dependency = dependencies[i]
-        var independentField = dependency.independent;
-        var dependentField = dependency.dependent;
-        if (Object.keys(interests).includes(independentField) || Object.keys(interests).includes(dependentField)){
-            var dependentFieldType = interests[dependentField];
-            if (dependentFieldType == "datetime" || dependentFieldType == "string"){
-                // The dependent field is not a numeric field.
-                continue;
-            }
-            var independentFieldType = interests[independentField];
-            if (independentFieldType == "datetime"){
-                var chartType = "line";
-            }
-            else if (independentFieldType == "string"){
-                var chartType = "bar";
-            }
-            else{
-                var chartType = "scatter";
-            }
-            var chart = new Chart();
-            chart.setType(chartType);
-            var x = [];
-            var y = [];
-            Story.instance.data.rawData.forEach(function(datum){
-                x.push(datum[independentField]);
-                y.push(datum[dependentField]);
-            });
-            // calculate the average value of each x
-            if (chartType == "bar"){
-                var x_unique_sum = {};
-                var x_unique_size = {};
-                for(var i=0; i<x.length; i++){
-                    var y_value = parseFloat(y[i]);
-                    if (x[i] in x_unique_sum){
-                        x_unique_sum[x[i]] += y_value;
-                        x_unique_size[x[i]] += 1;
-                    } 
-                    else{
-                        x_unique_sum[x[i]] = y_value;
-                        x_unique_size[x[i]] = 1;
-                    }
-                }
-                x = [];
-                y = [];
-                for(var k in x_unique_sum){
-                   x.push(k); 
-                   y.push(x_unique_sum[k] / x_unique_size[k]);
-                }
-            }
+  var charts = [];
+  for (var i = 0; i < dependencies.length; i++) {
+    dependency = dependencies[i];
+    var independentField = dependency.independent;
+    var dependentField = dependency.dependent;
 
-            chart.setX(x);
-            chart.setXLabel(independentField);
-            chart.setY(y);
-            chart.setYLabel(dependentField);
-            chart.setTitle(dependentField.charAt(0).toUpperCase() + dependentField.slice(1)+" vs "+independentField)
-            charts.push(chart);
+    if (
+      Object.keys(interests).includes(independentField) &&
+      Object.keys(interests).includes(dependentField)
+    ) {
+      var dependentFieldType = interests[dependentField];
+      if (dependentFieldType == "datetime" || dependentFieldType == "string") {
+        // The dependent field is not a numeric field.
+        continue;
+      }
+      var independentFieldType = interests[independentField];
+      if (independentFieldType == "datetime") {
+        var chartType = "line";
+      } else if (independentFieldType == "string") {
+        var chartType = "bar";
+      } else {
+        var chartType = "scatter";
+      }
+      var chart = new Chart();
+      chart.setType(chartType);
+      var x = [];
+      var y = [];
+      Story.instance.data.rawData.forEach(function(datum) {
+        x.push(datum[independentField]);
+        y.push(datum[dependentField]);
+      });
+      // calculate the average value of each x
+      if (chartType == "bar") {
+        var x_unique_sum = {};
+        var x_unique_size = {};
+        for (var j = 0; j < x.length; j++) {
+          var y_value = parseFloat(y[j]);
+          if (x[j] in x_unique_sum) {
+            x_unique_sum[x[j]] += y_value;
+            x_unique_size[x[j]] += 1;
+          } else {
+            x_unique_sum[x[j]] = y_value;
+            x_unique_size[x[j]] = 1;
+          }
         }
+        x = [];
+        y = [];
+        for (var k in x_unique_sum) {
+          x.push(k);
+          y.push(x_unique_sum[k] / x_unique_size[k]);
+        }
+      }
+
+      chart.setX(x);
+      chart.setXLabel(independentField);
+      chart.setY(y);
+      chart.setYLabel(dependentField);
+      chart.setTitle(
+        dependentField.charAt(0).toUpperCase() +
+          dependentField.slice(1) +
+          " vs " +
+          independentField
+      );
+      charts.push(chart);
+      console.log(charts);
     }
-    return charts;
+  }
+  return charts;
 }
 
-function chartsInterests(){
-    var charts = [];
-    Object.keys(Story.instance.metadata.interests).forEach(function(interest){
-        var t = Story.instance.metadata.interests[interest];
-        if(t == "float" || t == "integer"){
-            chart = new Chart();
-            chart.setType("bar");
-            values = [];
-            Story.instance.data.rawData.forEach(function(datum){
-                values.push(datum[interest]);
-            });
-            chart.setY(values);
+function chartsInterests() {
+  var charts = [];
+  Object.keys(Story.instance.metadata.interests).forEach(function(interest) {
+    var t = Story.instance.metadata.interests[interest];
+    if (t == "float" || t == "integer") {
+      chart = new Chart();
+      chart.setType("bar");
+      values = [];
+      Story.instance.data.rawData.forEach(function(datum) {
+        values.push(datum[interest]);
+      });
+      chart.setY(values);
 
-            //chart.setX(Array(values.length));
-            labels = [];
-            for(let i=0; i<values.length; i++){
-                labels[i] = i;
-            }
-            chart.setX(labels);
-            chart.setXLabel(interest);
-            chart.setTitle("Value of "+interest);
-            charts.push(chart);
-
-        }
-    });
-    return charts;
+      //chart.setX(Array(values.length));
+      labels = [];
+      for (let i = 0; i < values.length; i++) {
+        labels[i] = i;
+      }
+      chart.setX(labels);
+      chart.setXLabel(interest);
+      chart.setTitle("Value of " + interest);
+      charts.push(chart);
+    }
+  });
+  return charts;
 }
-
